6d5fada8b3c998f46586ed2b51abcd43d7591cfc
Shamil Magomedov <shomainkognito@yandex.ru>
2023-05-15 17:41:04 +0300

    push project for gh-pages

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-import { isWebp } from './modules/utils.js';
-import './modules/lazyload.js';
-import './modules/slider.js';
-import './modules/menu.js';
-import './modules/favorite.js';
-import './modules/basket.js';
-import './modules/change-colors.js';
-
-isWebp();
--- src/js/modules/menu.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.querySelector('.menu');
-const header = document.querySelector('.header');
-
-menuBtn.addEventListener('click', (evt) => {
-  evt.currentTarget.classList.toggle('menu--active');
-  header.classList.toggle('header--opened');
-});
